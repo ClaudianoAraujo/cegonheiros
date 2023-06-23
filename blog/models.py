c802083d8cd1af6d05880ec45180b78e49944183
Claudiano <claudiano.geek@gmail.com>
2023-06-22 21:50:03 -0300

    update

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     sbtitle = models.CharField(max_length=255, null=True, blank=True)
     content = models.TextField(null=False, blank=False)
+    content_1 = models.TextField(null=True, blank=True)
+    content_2 = models.TextField(null=True, blank=True)
+    content_3 = models.TextField(null=True, blank=True)
+    content_4 = models.TextField(null=True, blank=True)
+    content_5 = models.TextField(null=True, blank=True)
+    content_6 = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.title
